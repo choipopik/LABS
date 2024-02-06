6b385fdc59d7d4121a5a0b1e50b2122bea6d4be9
choipopik <sneakymail228@gmail.com>
2024-02-06 23:47:54 +0200

    sm upd

--- Python/pytlab1/task2.py
+++ Python/pytlab1/task2.py
@@ -1,10 +1,6 @@
 # task 2
 d = int(input())
-s = ""
-i = 0
-jd = len(str(d))
-while d > 0:
-    i += 1
-    s += str(i) + (" " * (jd - len(str(i))))
-    print(s)
-    d -= 1
+for i in range(d+1):
+    for j in range(1,i+1):
+        print(j,end="_")
+    print()
--- Python/pytlab1/task3.py
+++ Python/pytlab1/task3.py
@@ -1,12 +1,24 @@
-# task 3
-d = int(input()) - 1
-s = "1"
-i = 1
-jd = len(str(d+1))
-print((d * " ") + s)
-while d > 0:
-    i += 1
-    std_ots = (" " * (d - 1))
-    s = str(i) + s + str(i)
-    print(std_ots + s)
-    d -= 1
+## task 3
+#    d = int(input()) - 1
+#    s = "1"
+#    i = 1
+#    jd = len(str(d+1))
+#    print((d * " ") + s)
+#    while d > 0:
+#        i += 1
+#        std_ots = (" " * (d - 1))
+#        s = str(i) + s + str(i)
+#        print(std_ots + s)
+#        d -= 1
+d = int(input())
+k = d
+for i in range(d+1):
+    print(" " * k, end="")
+    for j in range(i, 0, -1):
+        print(j, end="")
+    for l in range(2, i+1):
+        print(l, end="")
+    k -= 1
+    print()
+
+
