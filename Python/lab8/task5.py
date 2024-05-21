e6d34bdc8af19167f1d679790f92a15a3851efdc
choipopik <sneakymail228@gmail.com>
2024-05-21 23:01:52 +0200

    Update task5.py

--- Python/lab8/task5.py
+++ Python/lab8/task5.py
@@ -1,10 +1,26 @@
-import matplotlib.pyplot as plt
 import numpy as np
-
-figure, axes = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
-
-#ЧТО ТАКОЕ функции среднеквадратичного отклонения MSE. КАК ЕЁ НАРИСОВАТЬ#
+import matplotlib.pyplot as plt
 
 
+fig, ax = plt.subplots(1, 2, subplot_kw={'projection': '3d'})
+
+x1 = np.linspace(0, 10, 100)
+y1 = np.random.normal(0, 1, 100)
+z1 = np.square(y1)
+
+x2 = np.linspace(0, 10, 100)
+y2 = np.random.normal(0, 1, 100)
+z2 = np.square(y2)
+
+ax[0].plot_trisurf(x1, y1, z1)
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('y')
+ax[0].set_zlabel('z')
+
+ax[1].plot_trisurf(x2, y2, z2)
+ax[1].set_xlabel('x')
+ax[1].set_ylabel('y')
+ax[1].set_zlabel('z')
+ax[1].set_zscale('log')
 
 plt.show()
